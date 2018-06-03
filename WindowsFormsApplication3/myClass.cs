38178bac985d5b47e4a9c84ae276fe07ac9408d0
liankun <liankun1994@live.com>
2018-06-03 12:42:07 +0800

    master4

--- WindowsFormsApplication3/myClass.cs
+++ WindowsFormsApplication3/myClass.cs
@@ -11,8 +11,8 @@ namespace WindowsFormsApplication3
         //xxxxxxxxxxxxxxxxx
         //ok
         /* 
-         * 创建本地库、创建master分支
-         * 创建远程库、发布master分支
+         * 创建本地库
+         * 创建远程库
          * 暂存更改
          * 提交更改
          * 回退本地版本
@@ -20,11 +20,13 @@ namespace WindowsFormsApplication3
          * 创建分支
          * 合并分支
          * 解决冲突
+         * 删除分支
          * 
          * master(正式版)---------------
          * dev(测试版)------------------
          * zlk(个人分支)----------------
          * xx(个人分支)-----------------
+         * temp(临时分支)---------------
          */
         
     }
